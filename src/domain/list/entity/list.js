99f241594bf126fc2a4a45c1fb432108303f0c2f
jorgeglezca98 <jorgeglezca98@gmail.com>
2020-10-30 11:47:57 +0000

    Creating List entity

--- src/domain/list/entity/list.js
+++ src/domain/list/entity/list.js
@@ -1,12 +1,13 @@
 
 class List {
 
-    constructor(id, author, name, likes, elements) {
-        this.id = id;
-        this.author = author;
-        this.name = name;
-        this.likes = likes;
-        this.elements = elements;
+    constructor(id, author, name, category, likes, elements) {
+        this.id = id
+        this.author = author
+        this.name = name
+        this.category = category
+        this.likes = likes
+        this.elements = elements
     }
 }
 
