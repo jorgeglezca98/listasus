25582eb934d3d7157efa2b3d7abdbf23270566e6
jorgeglezca98 <jorgeglezca98@gmail.com>
2020-11-14 18:25:24 +0000

    SearchPage component from class to hook

--- src/pages/search/searchPage.tsx
+++ src/pages/search/searchPage.tsx
@@ -12,48 +12,53 @@ interface SearchPageProps extends RouteComponentProps {
 }
 
 
-class SearchPage extends React.Component<SearchPageProps> {
-
-    state: { searchResults: Array<ListEntity> }
-
-    constructor (props: SearchPageProps) {
-        super(props)
-        if(props.search) {
-             // search results and save them
-             this.state = { searchResults: [] }
-        } else {
-            this.state = {
-                searchResults : Array(20).fill(
-                    new ListEntity(1, "Jorge", "Piezas de ordenador", "technology", 123432, ["Placa base", "RAM", "Procesador", "Gráfica"] )
-                )
-            }
-        }
+const SearchPage = (props: SearchPageProps) => {
+    /* State */
+    let initialResults : Array<ListEntity>
+    if(props.search) {
+        // search results and save them
+        initialResults = []
+    } else {
+        initialResults = Array(20).fill(
+            new ListEntity(1, "Jorge", "Piezas de ordenador", "technology", 123432, ["Placa base", "RAM", "Procesador", "Gráfica"] )
+        )
     }
+    const [ searchResults, changeSearchResults ] = React.useState<Array<ListEntity>>(initialResults)
 
-    searchResults = (search: string) => { }
+    /* Methods */
 
-    showExtendedList = (list: ListEntity) => {
-        this.props.history.push({
+    const searchForResults = (search: string) => { }
+
+    const showExtendedList = (list: ListEntity) => {
+        props.history.push({
             pathname: `/search/${list.getId}`,
             state: { list }
         })
     }
 
-    render () {
-        return (
-            <div className={styles.searchResults}>
-                <div className={ styles.searchBarContainer }>
-                    <SearchBar onSearch={ this.searchResults }/>
-                </div>
-                <p>{this.state.searchResults.length === 0 ? "No lists found" : `${this.state.searchResults.length} results` }</p>
-                <div className={styles.listsContainer}>
-                    { this.state.searchResults.map((list: ListEntity) => 
-                    ( <List id={list.getId} author={list.getAuthor} name={list.getName} category={list.getCategory} likes={list.getLikes} elements={list.getElements} onClick={ () => this.showExtendedList(list) }/> )
-                    )}
-                </div>
+    /* Render */
+
+    return (
+        <div className={styles.searchResults}>
+            <div className={ styles.searchBarContainer }>
+                <SearchBar onSearch={ searchForResults }/>
             </div>
-        )
-    }
+            <p>{ searchResults.length === 0 ? "No lists found" : `${searchResults.length} results` }</p>
+            <div className={styles.listsContainer}>
+                { searchResults.map((list: ListEntity) => 
+                ( <List 
+                    id={list.getId} 
+                    author={list.getAuthor} 
+                    name={list.getName} 
+                    category={list.getCategory} 
+                    likes={list.getLikes} 
+                    elements={list.getElements} 
+                    onClick={ () => showExtendedList(list) }
+                /> )
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default SearchPage
