b44a0478c55742fca435e76062c2c810f2c2349a
jorgeglezca98 <jorgeglezca98@gmail.com>
2020-11-15 12:30:24 +0000

    FullList component from class to hook

--- src/pages/fullList/fullList.tsx
+++ src/pages/fullList/fullList.tsx
@@ -10,39 +10,32 @@ import styles from './fullList.module.css'
 
 interface FullListPageProps extends RouteComponentProps<any, any,{ list?: ListEntity }> {}
 
-
-class FullListPage extends React.Component<FullListPageProps> {
-
-    private list: ListEntity
-
-    constructor(props: FullListPageProps) {
-        super(props)
-
-        if(!props.location.state || !props.location.state.list) {
-            // search data by id and update the list attributes
-            this.list = new ListEntity(1, "Jorge", "Piezas de ordenador", "technology", 123432, ["Placa base", "RAM", "Procesador", "Gráfica"] )
-        } else {
-            this.list = props.location.state.list
-        }
+const FullListPage = (props: FullListPageProps) => {
+    let givenList;
+    if(!props.location.state?.list) {
+        // search data by id and update the list attributes
+        givenList = new ListEntity(1, "Jorge", "Piezas de ordenador", "technology", 123432, ["Placa base", "RAM", "Procesador", "Gráfica"] )
+    } else {
+        givenList = props.location.state.list
     }
 
-    render() {
-        return (
-            <div className={ styles.listBackground } onClick={ () => this.props.history.replace("/search") }>
-                <div className={ styles.listContainer } onClick={ (event) => { event.stopPropagation() } }>
-                <List 
-                    type="extended"
-                    id={ this.list.getId }
-                    author={ this.list.getAuthor } 
-                    name={ this.list.getName } 
-                    category={ this.list.getCategory } 
-                    likes={ this.list.getLikes } 
-                    elements={ this.list.getElements }
-                />
-                </div>
+    const [list, changeList] = React.useState<ListEntity>(givenList)
+
+    return (
+        <div className={ styles.listBackground } onClick={ () => props.history.replace("/search") }>
+            <div className={ styles.listContainer } onClick={ (event) => { event.stopPropagation() } }>
+            <List 
+                type="extended"
+                id={ list.getId }
+                author={ list.getAuthor } 
+                name={ list.getName } 
+                category={ list.getCategory } 
+                likes={ list.getLikes } 
+                elements={ list.getElements }
+            />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default FullListPage
